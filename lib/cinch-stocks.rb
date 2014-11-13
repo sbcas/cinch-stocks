4e39a864f138a6efa54bf5a26e6d14dd0a261606
Sam Cassiba <sam@cassiba.com>
2014-11-12 22:03:10 -0800

    fix cinch-stocks.rb, move stocks.rb back to its rightful place

--- lib/cinch-stocks.rb
+++ lib/cinch-stocks.rb
@@ -1,37 +1,4 @@
-# *-* coding: utf-8 -*-
-require 'cinch'
-require 'cinch/cooldown'
-require 'time-lord'
-require 'stock_quote'
-
-module Cinch
-  module Plugins
-    # Cinch Plugin to report stock prices
-    class Stocks
-      include Cinch::Plugin
-
-      enforce_cooldown
-
-      self.help = 'Use .stock <symbol> to see the current market price of a given stock.'
-
-      def initialize(*args)
-        super
-      end
-
-      match(/(?:s|stock) (.+)/, method: :stock)
-
-      def stock(m, query)
-        m.reply get_stock(query)
-      end
-
-      private
-
-      def get_stock(query)
-        stock = Stock.new(query).to_s
-        stock
-      rescue ArgumentError
-        "Sorry, couldn't find #{query}."
-      end
-    end
-  end
-end
+# -*-  coding: utf-8 -*-
+require 'cinch/plugins/stocks/version'
+require 'cinch/plugins/stocks'
+require 'cinch/plugins/stocks/stocks'
